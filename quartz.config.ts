1617da12db6c27d35ef77fdd5ad530da5b5b4731
jiarenyou <1528587698@qq.com>
2025-08-10 22:37:42 +0800

    test

--- quartz.config.ts
+++ quartz.config.ts
@@ -5,6 +5,7 @@ import * as Plugin from "./quartz/plugins"
  * Quartz 4 Configuration
  *
  * See https://quartz.jzhao.xyz/configuration for more information.
+ * "https://blog.oteneto.xyz"
  */
 const config: QuartzConfig = {
   configuration: {
